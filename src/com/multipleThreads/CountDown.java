08fa5bd5c1ad91c4d4aeeedcb554a19a7d13abe9
roopikasrinivas <roopika.srinivas@gmail.com>
2021-06-21 21:45:40 +0530

    Threads

---
+++ src/com/multipleThreads/CountDown.java
@@ -0,0 +1,45 @@
+package com.multipleThreads;
+
+import com.basics.ThreadColor;
+
+public class CountDown {
+    private static String m = "";
+    private int i;
+
+    public void doCountDown() {
+        String color;
+        switch (Thread.currentThread().getName()) {
+            case "Thread 1":
+                color = ThreadColor.ANSI_PURPLE;
+                break;
+            case "Thread 2":
+                color = ThreadColor.ANSI_CYAN;
+                break;
+            default:
+                color = ThreadColor.ANSI_RED;
+                break;
+        }
+
+        synchronized (m) {
+            for (i = 10; i > 0; i--) {
+                System.out.println(color + Thread.currentThread().getName() + ": i = " + i);
+            }
+        }
+    }
+
+}
+
+class CountDownThread extends Thread {
+
+    private CountDown countDown;
+
+    public CountDownThread(CountDown countDown) {
+        this.countDown = countDown;
+    }
+
+    @Override
+    public void run() {
+        countDown.doCountDown();
+    }
+
+}
